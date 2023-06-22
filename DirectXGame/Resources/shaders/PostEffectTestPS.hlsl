2c86f877afaa914634301b7c72b5168395c146e8
NishizawaYuga <k021g1246@g.neec.ac.jp>
2023-06-22 12:44:28 +0900

    単純なポストエフェクト

--- DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
+++ DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
@@ -5,7 +5,14 @@ SamplerState smp : register(s0);      // 0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	float4 texcolor = tex.Sample(smp,input.uv);
+	//UV指定したピクセルの色をサンプリング
+	//float4 texcolor = tex.Sample(smp,input.uv);
 
+	float4 texcolor = tex.Sample(smp,input.uv + float2(0.5f,0));			//UVずらし
+
+	//通常
 	return float4(texcolor.rgb, 1);
+	//return float4(texcolor.rgb * 2.0f, 1);			//RGBの数値を2倍して出力（明度上げ）
+	
+	//return float4(float3(1,1,1) - texcolor.rgb, 1);	//色反転
 }
