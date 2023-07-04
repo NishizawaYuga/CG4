4561edfd4f7ea6bb1c9b3ed88b21c0ffdbc737d8
NishizawaYuga <k021g1246@g.neec.ac.jp>
2023-07-04 12:56:39 +0900

    単純なポストエフェクト

--- DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
+++ DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
@@ -12,26 +12,28 @@ float4 main(VSOutput input) : SV_TARGET
 	//float4 texcolor = tex.Sample(smp,input.uv + float2(0.5f,0));
 
 	//UV座標それぞれの変化量1pixel分を計算して変数に入れておく
-	float posU = 1 / 1280;
-	float posV = 1 / 720;
+	float posU = 1.0f / 1280.0f;
+	float posV = 1.0f / 720.0f;
 	//色合計用の変数初期化
-	float4 totalColor = { 0,0,0,0 };
+	float4 texColor = { 0,0,0,0 };
 
-	float total = 0;
+	float total = 0.0f;
+	const float roopNum = 5.0f;
 
 	//平均ぼかし
-	for (int y = -1; y > 1; y++) {
-		for (int x = -1; x > 1; x++) {
-			totalColor += tex.Sample(smp, input.uv + float2(posU * x ,posV * y));
+	for (float y = -roopNum; y < roopNum; y += 1.0f) {
+		for (float x = -roopNum; x < roopNum; x += 1.0f) {
+			texColor += tex.Sample(smp, input.uv + float2(posU * x, posV * y));
+			total += 1.0f;
 		}
 	}
 
 	//通常
 	//return float4(texcolor.rgb, 1);
-	
+
 	//return float4(texcolor.rgb * 2.0f, 1);			//RGBの数値を2倍して出力（明度上げ）
-	
+
 	//return float4(float3(1,1,1) - texcolor.rgb, 1);	//色反転
 
-	return float4(totalColor.rgb, 1);				//平均ぼかし
+	return float4(texColor.rgb / total , 1);				//平均ぼかし
 }
