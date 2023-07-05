2a2f46c9f5e7e94d1d5690fccc3225a1a646f2fe
NishizawaYuga <k021g1246@g.neec.ac.jp>
2023-07-05 14:23:54 +0900

    マルチレンダーターゲット

--- DirectXGame/2d/PostEffect.h
+++ DirectXGame/2d/PostEffect.h
@@ -58,7 +58,7 @@ private:	//静的メンバ変数
 
 private:    //メンバ変数
 	//テクスチャバッファ
-	ComPtr<ID3D12Resource> texBuff;
+	ComPtr<ID3D12Resource> texBuff[2];
 	//SRV用デスクリプタヒープ
 	ComPtr<ID3D12DescriptorHeap> descHeapSRV;
 	//深度バッファ
